6bcff938d41b5558a4f992795fea1ff15b8a106f
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2024-09-26 11:03:45 -0500

    Comunicacion ventanas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { EventCardComponent } from './event-card/event-card.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
+import { AdminDashBoardComponent } from './admin-dash-board/admin-dash-board.component';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
+  {path: 'evento', component:EventCardComponent},
+  {path: 'adminHeader', component:AdminHeaderComponent},
+  {path: 'adminDashBoard', component:AdminDashBoardComponent},
+  {path: 'user', component:UserComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import {RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule ],
+  imports: [RouterModule,FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor() {}
-  loggedIn=false;
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
   onSubmit() {
-    // Aquí puedes manejar el evento de envío del formulario
-    console.log('Formulario enviado');
+    if (this.username === 'admin' && this.password === '1234') {
+      // Redirige a adminDashBoard si las credenciales son correctas
+      this.router.navigate(['/adminDashBoard']);
+    }else if(this.username === 'juan' && this.password === '1234') {
+      this.router.navigate(['/user']);  
+    }else {
+      // Maneja el error si las credenciales son incorrectas
+      alert('Usuario o contraseña incorrectos');
+    }
   }
-
-  greet(){
-    this.loggedIn=true;
-    alert('holi');
-  }
-  openRegister(){
-
-  }
-
 }
